5a37723f50f9ffb863438288ca5662c762234cf6
Eric Abell <ericabell@users.noreply.github.com>
2017-07-26 17:09:59 -0500

    Address class implemented

---
+++ src/Branch.java
@@ -0,0 +1,36 @@
+public class Branch {
+    Address address;
+    String hours;
+
+    public Branch() {
+    }
+
+    public Branch(String addressLine1, String addressLine2, String city, String stateAbbr, String zip, String hours) {
+        this.address = new Address(addressLine1, addressLine2, city, stateAbbr, zip);
+        this.hours = hours;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getHours() {
+        return hours;
+    }
+
+    public void setHours(String hours) {
+        this.hours = hours;
+    }
+
+    @Override
+    public String toString() {
+        return "Branch{" +
+                "address=" + address +
+                ", hours='" + hours + '\'' +
+                '}';
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Member member1 = new Member("John Doe", "123 Main Str", "Apt 234", "Austin", "TX", "78709", "1999-02-23");
 
+        // CREATE OBJECTS FOR MEMBER AND BRANCH
+        Member member1 = new Member("John Doe", "123 Main Str", "Apt 234", "Austin", "TX", "78709", "1999-02-23");
+        Branch branch1 = new Branch("321 Main Str", "Apt 4567", "Austin", "TX", "78709", "9am-5pm");
+
+        // PRINT THEM USES toString
         System.out.println(member1);
+        System.out.println(branch1);
+
+        // USE GETTER/SETTER
+        System.out.println( "member1's address is " + member1.getAddress() );
+        Address newAddress = new Address("123 Main Street", "Apt 543", "Austin", "TX", "78709");
+        member1.setAddress(newAddress);
+        System.out.println( "member1's city after setter is " + member1.getAddress() );
+
 
     }
 }
